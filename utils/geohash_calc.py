a4b97619b9e11fe7af57cb30c93d5e242cd0505e
hjpark625 <zero950@gmail.com>
2024-07-08 17:44:17 +0900

    refactor: latitude, longitude float형으로 타입 캐스팅

--- utils/geohash_calc.py
+++ utils/geohash_calc.py
@@ -4,7 +4,9 @@ import geohash2
 def calculate_geohash(row: dict[str, str]):
     if row["latitude"] != "NULL" and row["longitude"] != "NULL":
         return geohash2.encode(
-            latitude=row["latitude"], longitude=row["longitude"], precision=8
+            latitude=float(row["latitude"]),
+            longitude=float(row["longitude"]),
+            precision=8,
         )
     else:
         return "NULL"
